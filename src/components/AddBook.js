d442bcb2d9b3d7366c737b83077436d27509c294
Sanja <sanja1mandic@gmail.com>
2022-07-12 19:20:05 +0200

    Change submit input to be button, and add function for adding book,  with ttile and Author properties from imput fields

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const AddBook = () => (
-  <div className="w-75">
-    <h3 className="">Add new book</h3>
-    <form className="d-flex justify-content-between">
-      <input className="w-50" type="text" name="title" placeholder="Book title" />
-      <input className="" type="text" name="author" placeholder="Book author" />
-      <input className="btn btn-primary" type="submit" value="Add Book" />
-    </form>
-  </div>
-);
+const AddBook = (props) => {
+  let title = '';
+  let author = '';
+
+  const { addBook } = props;
+
+  const addTitle = (e) => {
+    title = e.target.value;
+  };
+
+  const addAuthor = (e) => {
+    author = e.target.value;
+  };
+  const submitBook = () => {
+    addBook(title, author);
+  };
+  return (
+    <div className="w-75">
+      <h3 className="">Add new book</h3>
+      <form className="d-flex justify-content-between">
+        <input className="w-50" type="text" name="title" placeholder="Book title" onChange={addTitle} />
+        <input className="" type="text" name="author" placeholder="Book author" onChange={addAuthor} />
+        <button className="btn btn-primary" type="button" value="Add Book" onClick={submitBook}>Add Book</button>
+      </form>
+    </div>
+  );
+};
+
+AddBook.propTypes = {
+  addBook: PropTypes.func.isRequired,
+};
 
 export default AddBook;
