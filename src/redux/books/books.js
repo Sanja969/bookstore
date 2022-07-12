3d52893b46cb4f0f3596955b4d297776732660e7
Sanja <sanja1mandic@gmail.com>
2022-07-12 19:28:23 +0200

    Change initial state to have some objects in array instead of empty array

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,26 +1,42 @@
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
+const initialState = [
+  {
+    id: 23245,
+    title: 'The Hunger Games',
+    author: 'Suzzane Collins',
+  },
+  {
+    id: 324254333,
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    id: 3534545645,
+    title: 'Capital in the 21st Century',
+    author: 'Suzzane Collins',
+  },
+];
 
-export default function booksReducer(state = [], action = {}) {
+const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD:
       return [...state, action.book];
     case REMOVE:
       return state.filter((book) => book.id !== action.id);
-    default: return state;
+    default:
+      return state;
   }
-}
+};
 
-export function addBook(book) {
-  return {
-    type: ADD,
-    book,
-  };
-}
+export default booksReducer;
 
-export function removeBook(id) {
-  return {
-    type: REMOVE,
-    id,
-  };
-}
+export const addBook = (book) => ({
+  type: ADD,
+  book,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE,
+  id,
+});
