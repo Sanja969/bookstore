c9886e4dc3d8cab9dc2d88b89d73a8151b4b1cd6
Sanja <sanja1mandic@gmail.com>
2022-07-12 19:26:34 +0200

    Imported useDispatch  and checkStatus action and add it when check button is clicked

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div className="mt-5 pt-5 ms-3">
-    <button type="button" className="btn btn-primary">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const check = () => dispatch(checkStatus());
+  return (
+    <div className="mt-5 pt-5 ms-3">
+      <button type="button" className="btn btn-primary" onClick={check}>Check status</button>
+    </div>
+  );
+};
 
 export default Categories;
