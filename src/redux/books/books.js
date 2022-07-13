57d3a730d9939dd685dfba5fc386fa0a2d6de36e
Sanja <sanja1mandic@gmail.com>
2022-07-13 15:59:42 +0200

    Change initial state to be empty andook functions, for storing data to bookstore API, and for geting that data, triger those async functions when actions occurs

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,27 +1,15 @@
-const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
-const initialState = [
-  {
-    id: 23245,
-    title: 'The Hunger Games',
-    author: 'Suzzane Collins',
-  },
-  {
-    id: 324254333,
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: 3534545645,
-    title: 'Capital in the 21st Century',
-    author: 'Suzzane Collins',
-  },
-];
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
+const SAVE_BOOK = 'bookStore/books/SAVE_BOOK';
+const initialState = [];
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/rSWOuh8Pfu8EVUz1p7ix/books';
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD:
+    case SAVE_BOOK:
       return [...state, action.book];
+    case GET_BOOKS:
+      return action.books;
     case REMOVE:
       return state.filter((book) => book.id !== action.id);
     default:
@@ -31,12 +19,49 @@ const booksReducer = (state = initialState, action) => {
 
 export default booksReducer;
 
-export const addBook = (book) => ({
-  type: ADD,
-  book,
-});
+export const removeBook = (id) => async (dispatch) => {
+  const bookDelete = await fetch(`${url}/${id}`, { method: 'DELETE' });
+  const response = await bookDelete.text();
+  if (response) {
+    dispatch({
+      type: REMOVE,
+      id,
+    });
+  }
+};
 
-export const removeBook = (id) => ({
-  type: REMOVE,
-  id,
-});
+export const saveBook = (book) => async (dispatch) => {
+  fetch(url,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: book.id,
+        title: book.title,
+        author: book.author,
+        category: 'underfined',
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  dispatch({
+    type: SAVE_BOOK,
+    book,
+  });
+};
+
+export const getBooks = () => async (dispatch) => {
+  const response = await fetch(url);
+  const json = await response.json();
+  const entries = Object.entries(json);
+  const books = entries.map((element) => ({
+    id: element[0],
+    title: Object.assign(...element[1]).title,
+    author: Object.assign(...element[1]).author,
+  }));
+
+  dispatch({
+    type: GET_BOOKS,
+    books,
+  });
+};
